aae9566953093d4e60698e3da4dd5918549de341
Naomi <n.valdovinos74@gmail.com>
2021-06-30 07:26:37 -0400

    solved error unhandled promise error

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,9 @@
 require('dotenv').config();
-
 const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(
-    DB_Name = 'ecommerce_db',
-    DB_USER = 'root',
-    DB_PW = 'Rm55ilyuh',{
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     dialectOptions: {
